e7938999a2184e9d6f80f3ec585024cb1d015a23
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-04-27 15:36:02 +0530

    update

--- webD/script.js
+++ webD/script.js
@@ -3,15 +3,21 @@ let postHrt = document.querySelector(".post-heart");
 let postImg = document.querySelector(".post img");
 let like = false;
 
-postImg.addEventListener("dblclick", () => {
-    postHrt.style.display = "block";
+postImg.addEventListener("click", () => {
+    postHrt.style.trasform = "scale(1)";
+    postHrt.style.opacity = 0.8;
 
     setTimeout(() => {
-        postHrt.style.display = "none";
+        postHrt.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
+        postHrt.style.trasform = "scale(0)";
     }, 2000);
+
     hrt.classList.remove("ri-heart-line");
     hrt.classList.add("ri-heart-fill");
-})
+});
 
 
 hrt.addEventListener("click", function () {
