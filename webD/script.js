5c6d62aa94f7841106cf8d7c4b93b37817e84577
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-04-27 14:23:22 +0530

    update

--- webD/script.js
+++ webD/script.js
@@ -1,15 +1,28 @@
 let hrt = document.querySelector(".heart");
+let postHrt = document.querySelector(".post-heart");
+let postImg = document.querySelector(".post img");
 let like = false;
 
+postImg.addEventListener("dblclick", () => {
+    postHrt.style.display = "block";
+
+    setTimeout(() => {
+        postHrt.style.display = "none";
+    }, 2000);
+    hrt.classList.remove("ri-heart-line");
+    hrt.classList.add("ri-heart-fill");
+})
+
+
 hrt.addEventListener("click", function () {
-    if (like) {
-        hrt.classList.remove("ri-heart-line");
-        hrt.classList.add("ri-heart-fill");
-        like = false;
-    }
-    else {
+    if (like == false) {
         hrt.classList.add("ri-heart-line");
         hrt.classList.remove("ri-heart-fill");
         like = true;
     }
-})
+    else {
+        hrt.classList.remove("ri-heart-line");
+        hrt.classList.add("ri-heart-fill");
+        like = false;
+    }
+});
