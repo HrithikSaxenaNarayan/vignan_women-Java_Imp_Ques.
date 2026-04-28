8b9e3d38dc0cdb759c9db3b891a2d7cd8bc8462c
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-04-28 15:17:48 +0530

    update

--- Java/arrays.java
+++ Java/arrays.java
@@ -16,17 +16,60 @@ public class arrays {
             System.out.println(arr[i]);
         }
     }
-   
+
     public static int maximum(int[] arr) {
-        int maxEle = -(int)1e9;
-        for(int elem : arr){
+        int maxEle = -(int) 1e9;
+        for (int elem : arr) {
             maxEle = Math.max(maxEle, elem);
         }
         return maxEle;
     }
-   
+
+    public static int minimum(int[] arr) {
+        int minEle = (int) 1e9;
+        for (int elem : arr) {
+            minEle = Math.min(elem, minEle);
+        }
+        return minEle;
+    }
+
+    public static int find(int[] arr, int data) {
+        int foundAtIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (data == arr[i]) {
+                foundAtIndex = i;
+            }
+        }
+        return foundAtIndex;
+    }
+
+    public static int spanOfArray1 (int[] arr) {
+        int minElem = minimum(arr);
+        int maxEle = maximum(arr);
+
+        return maxEle - minElem;
+    }
+
+    public static int spanOfArray2(int[] arr) {
+        int minEle = (int) 1e9;
+        int maxEle = -(int) 1e9;
+
+        for (int elem : arr) {
+            minEle = Math.min(minEle, elem);
+            maxEle = Math.max(maxEle, elem);
+        }
+        int span = maxEle - minEle;
+        return span;
+    }
+
+    public static int[] reverseOfArray(int[] arr) {
+        // no extra space
+        // no strings
+        // no standard liberaries
+        // within the time complexity of O(n)
+    }
+
     public static void main(String[] args) {
-        // display1(input1(scn.nextInt()));
-        System.out.println(maximum(input1(scn.nextInt())));
+        System.out.println(spanOfArray(input1(scn.nextInt())));
     }
 }
