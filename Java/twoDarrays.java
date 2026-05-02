9a9b7c96bbe0ef3e07829fae8a83a40a0fe09688
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-05-02 11:45:07 +0530

    update

--- Java/twoDarrays.java
+++ Java/twoDarrays.java
@@ -115,28 +115,28 @@ public class twoDarrays{
         int cmin = 0, cmax = m-1;
 
         while(tnel >= 0) {
-            for (int row = rmin; row <= rmax; row++) {
+            for (int row = rmin; row <= rmax && tnel > 0; row++) {
                 System.out.print(arr[row][cmin] + " ");
                 tnel--;
             }
             cmin++;
             System.out.println();
 
-            for (int col = cmin; col <= cmax; col++) {
+            for (int col = cmin; col <= cmax && tnel > 0; col++) {
                 System.out.print(arr[rmax][col] + " ");
                 tnel--;
             }
             rmax--;
             System.out.println();
 
-            for (int row = rmax; row >= rmin; row--) {
+            for (int row = rmax; row >= rmin && tnel > 0; row--) {
                 System.out.print(arr[row][cmax] + " ");
                 tnel--;
             }
             cmax--;
-            System.out.println();
+            System.out.println(); 
 
-            for (int col = cmax; col >= cmin; col--) {
+            for (int col = cmax; col >= cmin && tnel > 0; col--) {
                 System.out.print(arr[rmin][col] + " ");
                 tnel--;
             }
