1ee8403fde68a3d1a2577302eb10c2fbb60c095a
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-05-02 14:18:13 +0530

    update

--- Java/binarySearch.java
+++ Java/binarySearch.java
@@ -15,25 +15,64 @@ public class binarySearch {
 
     public static int searchData(int[] arr, int data) {
         int si = 0, ei = arr.length - 1;
-        
+
         while (si <= ei) {
             int mid = ((ei + si) / 2);
             if (arr[mid] == data) {
                 return mid;
-            }
-            else if (data < arr[mid]) {
-                ei = mid - 1 ;
-            }
-            else {
+            } else if (data < arr[mid]) {
+                ei = mid - 1;
+            } else {
                 si = mid + 1;
             }
         }
         return -1;
     }
 
-    
+    public static int frstIdx(int[] arr, int data) {
+        int si = 0;
+        int ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = ((si + ei) / 2);
+            if (arr[mid] == data) {
+                if (mid - 1 >= 0 && arr[mid - 1] == data) {
+                    ei = mid - 1;
+                } else {
+                    return mid;
+                }
+
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int lastIdx(int[] arr, int data) {
+        int si = 0;
+        int ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = ((si + ei) / 2);
+            if (arr[mid] == data) {
+                if (mid + 1 < arr.length && arr[mid + 1] == data) {
+                    si = mid + 1;
+                } else {
+                    return mid;
+                }
+
+            } else if (arr[mid] < data) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(searchData(input1(scn.nextInt()), scn.nextInt()));
+        System.out.println(lastIdx(input1(scn.nextInt()), scn.nextInt()));
     }
 }
 
